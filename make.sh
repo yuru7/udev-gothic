34f2dff757bf0b2ead21904620845d099111c515
yuru7 <13458509+yuru7@users.noreply.github.com>
2022-03-24 18:54:55 +0900

    Update to v0.0.3

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ BASE_DIR=$(cd $(dirname $0); pwd)
 WORK_DIR="$BASE_DIR/build_tmp"
 BUILD_DIR="$BASE_DIR/build"
 
-VERSION='0.0.2'
+VERSION='0.0.3'
 
 FAMILYNAME="UDEVGothic"
 DISP_FAMILYNAME="UDEV Gothic"
